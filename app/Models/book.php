181da04e7bf60a55655ccb8fa688259d9432f0a9
lufiana1503-a11y <lufiana1503@example.com>
2026-06-24 14:17:07 +0800

    menentukan relasi dan casts pada model book

--- app/Models/book.php
+++ app/Models/book.php
@@ -9,5 +9,22 @@ class book extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'category_id',
+        'title',
+        'writer',
+        'release_date'
+    ];
+
+    protected function casts(){
+
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function borrow(){
+        return $this->hasMany(Borrow::class);
+    }
 }
